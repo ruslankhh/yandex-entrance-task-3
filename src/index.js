7269d07d5f0fec53fd152354836f6fe60b446484
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-01-27 05:19:17 +0300

    fix: Replace Apollo client into index.js to App.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset';
-import { ApolloProvider, graphql } from 'react-apollo';
-
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { APP_QUERY } from './query/query';
-
-const client = new ApolloClient({
-  link: new HttpLink({ uri: '/graphql' }),
-  cache: new InMemoryCache()
-});
-
-const AppWithData = graphql(APP_QUERY)(({ data }) => (
-  <App data={data} />
-));
-
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <AppWithData />
-  </ApolloProvider>,
+  <App />,
   document.getElementById('root')
 );
+
 registerServiceWorker();
